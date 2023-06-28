879140b43b576c453852204cf09548ca2160edc8
3TERRRNITY <qertisww@gmail.com>
2023-06-28 20:33:31 +0300

    add search bar, install headless ui

---
+++ components/SearchBar.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+
+import { SearchManufacture } from "./";
+
+const SearchBar = () => {
+  const [manufacture, setManufacture] = useState("");
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacture
+          manufacturer={manufacture}
+          setManufacturer={setManufacture}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
