e9575b6cd9549eb586407b486628d488dc08355c
3TERRRNITY <qertisww@gmail.com>
2023-06-30 12:45:26 +0300

    add modal for car details

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { CarProps } from "../types";
 import CustomButton from "./CustomButton";
 import { calculateCarRent } from "../utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -76,6 +77,12 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
